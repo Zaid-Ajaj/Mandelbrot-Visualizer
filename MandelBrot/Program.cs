e6729b1978fff22df1d7f22b8006866cab29b8b4
Zaid-Ajaj <zaid.naom@gmail.com>
2015-07-13 17:45:13 +0200

    Added a comment

--- MandelBrot/Program.cs
+++ MandelBrot/Program.cs
@@ -9,7 +9,7 @@ namespace MandelBrot
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() 
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
